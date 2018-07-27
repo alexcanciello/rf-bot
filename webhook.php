d220cb27aadfc39c5715f93894d597393eba0c17
alexcanciello <41766583+alexcanciello@users.noreply.github.com>
2018-07-27 05:05:05 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,36 +1,61 @@
-+<?php
-+require_once (dirname(__FILE__) . '/config.php');
-+require_once (dirname(__FILE__) . '/FacebookBot.php');
-+$bot = new FacebookBot(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
-+$bot->run();
-+$messages = $bot->getReceivedMessages();
-+foreach ($messages as $message)
-+{
-+  $recipientId = $message->senderId;
-+	if($message->text)
-+	{
-+		$response = processRequest($message->text);
-+		$bot->sendTextMessage($recipientId, $response);
-+	}
-+}
-+
-+
-+function processRequest($text)
-+{
-+	$text = trim($text);
-+	$text = strtolower($text);
-+	$response = "";
-+	if($text=="domanda 1")
-+	{
-+		$response = "risposta alla domanda 1";
-+	}
-+	elseif ($text=="domanda 2")
-+	{
-+		$response = "risposta alla domanda 2";
-+	}
-+	else
-+	{
-+		$response = "Non capisco la domanda";
-+	}
-+	return $response;
-+} 
+<?php
+
+// parameters
+
+$hubVerifyToken = 'cloudwaysschool';
+
+$accessToken =   "EAxxxxxxxxxxxqgBAKWAgizvoHnQLZBR7ZxxxxxxxxxxxxxxxxxxxxxxxxxxxxxptYSymSdocFFCp1ink3EHRVMrCSxxxxxxxxxxxxxxxxxxxxwMZApStyA8GbqAxxxxxxxxxxxxxxxxxxxxxxxxxxx9R6QttFVyNS4ZBurwZDZD";
+
+// check token at setup
+
+if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+
+  echo $_REQUEST['hub_challenge'];
+
+  exit;
+
+}
+
+// handle bot's anwser
+
+$input = json_decode(file_get_contents('php://input'), true);
+
+$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+
+$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+
+$response = null;
+
+//set Message
+
+if($messageText == "hi") {
+
+    $answer = "Hello";
+
+}
+
+//send message to facebook bot
+
+$response = [
+
+    'recipient' => [ 'id' => $senderId ],
+
+    'message' => [ 'text' => $answer ]
+
+];
+
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+
+curl_setopt($ch, CURLOPT_POST, 1);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+if(!empty($input)){
+
+$result = curl_exec($ch);
+
+}
+
+curl_close($ch);
