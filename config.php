95c959e36c43ff863a7072ab0609afb7fb7d6d06
alexcanciello <41766583+alexcanciello@users.noreply.github.com>
2018-07-27 04:20:17 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 define("FACEBOOK_PAGE_ACCESS_TOKEN", "Insert here");
-define("FACEBOOK_PAGE_ID", "Insert here");
-define("FACEBOOK_VALIDATION_TOKEN", "Insert here");
+define("FACEBOOK_PAGE_ID", "1183045381841079");
+define("FACEBOOK_VALIDATION_TOKEN", "1029384756");
